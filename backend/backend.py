a7844e0ee7dcc914a9f3e6cf2118880fba2e5201
Paul Brozenec <paulbrozenec@gmail.com>
2018-07-22 20:59:25 -0700

    Added hashing function

--- backend/backend.py
+++ backend/backend.py
@@ -1,4 +1,5 @@
 import sqlite3
+from hashlib import *
 conn = sqlite3.connect('thelogin.db')
 c = conn.cursor()
 
@@ -8,8 +9,8 @@ def create_table(tablename):
 def create_table2(tablename):
     c.execute("CREATE TABLE IF NOT EXISTS "+tablename+" (user TEXT, appliance, TEXT)")
     
-create_table("login")
-create_table2("appliances")
+#create_table("login")
+#create_table2("appliances")
 
 def data_entry_login(username,password):
     c.execute("INSERT INTO login (user,password) VALUES(?,?)",(username,password))
@@ -18,8 +19,6 @@ def data_entry_login(username,password):
 def data_entry_appliances(owner,appliancename):
     c.execute("INSERT INTO login (user,appliance) VALUES(?,?)",(owner,appliancename))
     conn.commit()
-
-#data_entry_login("Edwin0101","Pawn1234")
 
 def login_password(username):
     c.execute("SELECT password FROM login WHERE user='"+username+"'")
@@ -30,3 +29,10 @@ def appliance_appliances(username):
     c.execute("SELECT appliance from appliances WHERE user='"+username+"'")
     for row in c.fetchall():
         return row[0]
+
+def encrypt(password):
+    m = sha512()
+    m.update(bytes(password,'utf-8'))
+    return m.digest()
+
+
