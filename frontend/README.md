6acdc2f3bf19acd70cb2cf6aef503765301f14ba
EanKeen <24364012+EanKeen@users.noreply.github.com>
2018-07-27 16:29:15 -0700

    Add ElementUI Not fully functional; default fonts do not work.

--- frontend/README.md
+++ frontend/README.md
@@ -7,10 +7,10 @@ npm install
 
 ### Compiles and hot-reloads for development
 ```
-npm run serve
+npm run dev
 ```
 
 ### Compiles and minifies for production
 ```
-npm run build
+npm run prod
 ```
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,16 @@
 import Vue from 'vue'
+
+// Fonts
+import '@/assets/fonts/fonts.scss'
+
+// Element UI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 import App from './App.vue'
+
+Vue.use(ElementUI)
+
 import router from './router'
 import store from './store'
 
